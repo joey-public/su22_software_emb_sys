81dcb5ea9d1cb9711fff12e59ffc08454b5bdd16
Joey <joeypublicgit@gmail.com>
2022-07-19 16:05:10 -0700

    trying to implement huffman encode

--- assignments/assignment1/huffman/src/huffman.cpp
+++ assignments/assignment1/huffman/src/huffman.cpp
@@ -7,9 +7,25 @@ void print_map(std::map<char, int> mp)
     std::cout << "KEY\tELEMENT\n";
     for (auto itr = mp.begin(); itr != mp.end(); ++itr) 
     {
-        std::cout << itr->first << '\t' << itr->second << '\n';
+        std::cout <<  itr->first << '\t' << itr->second << '\n';
     }
 }
+void print_queue(char *head, char *tail, std::map<char,int> mp)
+{
+    int len = tail-head;
+    std::cout << len << std::endl; 
+    std::cout << "KEY\tELEMENT\n";
+    for(int i = 0; i < (len-1); i++)
+    {
+        char key = *(head+i);
+//        std::cout << key << '\t' << mp.at(key) << '\n';
+        std::cout << key << '\n';
+    }
+}
+*char quickSortSymMap(std::map<char,int> mp)
+{
+}
+
 /**
  * TODO Complete this function
  **/
@@ -18,15 +34,44 @@ int huffman_encode(const unsigned char *bufin,
 						  unsigned char **pbufout,
 						  unsigned int *pbufoutlen)
 {
-std::cout << "Encoding Input File\n";
-    std::map<char, int> symbolTable = {};
-    for(int i = 0; i < bufinlen; i++)
+    std::cout << "Encoding Input File\n";
+    //1. calculate the frequency of each char 
+    std::map<char, int> symFreqMap = {};
+    for(int i = 0; i < bufinlen-1; i++)
     {
         char key = *(bufin + i);
-        if(symbolTable.count(key) == 0) { symbolTable.insert({key,1}); }
-        else{ symbolTable.at(key) += 1; }
+        if(symFreqMap.count(key) == 0) { symFreqMap.insert({key,1}); }
+        else{ symFreqMap.at(key) += 1; }
     }
-std::cout << "Finished Encoding Input File\n";
+    print_map(symFreqMap); 
+    //2.Sort the symbols from lease to most frequent aka min queue or minheap
+    char minSym = symFreqMap.begin()->first;
+    int minOcc = symFreqMap.begin()->second;
+    //head of queue should point to the least frequent sym 
+    char *head = &minSym;
+    char *tail = head;
+    for (auto itr = symFreqMap.begin(); itr != symFreqMap.end(); ++itr) 
+    {
+        char curSym = itr->first;
+        int curOcc = itr->second;
+        if (curOcc < minOcc)
+        {
+            std::cout << "Least Frequent is: " << curOcc << ":" << curSym;
+            std::cout << " Old was: " << symFreqMap.at(*head) << ":" << *head << std::endl;
+            head -= 1; 
+            *head = curSym;
+            minOcc = curOcc;
+        }
+        else
+        {
+            std::cout << "Tail is: " << curOcc << ":" << curSym;
+            tail += 1;
+            *tail = curSym;
+        }
+    }
+    std::cout << "\nTail - Head = " << tail-head << "\n";
+//    print_queue(head, tail, symFreqMap);
+    std::cout << "Finished Encoding Input File\n";
     return 1; 
 }
 
