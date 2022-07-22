7ba8a21e29d8b1def30daeddeced8329255110eb
Joey <joeypublicgit@gmail.com>
2022-07-21 23:25:22 -0700

    Fixed bugs so that adding and popping nodes from the heap works

--- assignments/assignment1/huffman/src/huffman.cpp
+++ assignments/assignment1/huffman/src/huffman.cpp
@@ -8,6 +8,7 @@ struct Node{
     bool isLeaf;
     char character;
     int frequency;
+    Node* parent;
 };
 struct NodeHeap{
     std::stack<int> availableKey;
@@ -30,16 +31,33 @@ void popMin(NodeHeap& heap, Node& minNode)
     minNode.isLeaf = min.isLeaf;
     minNode.character = min.character;
     minNode.frequency = min.frequency;
+//    std::cout << "\tPopping:" << minKey << ", " << min.character << ", " << min.frequency << std::endl;
     heap.map.erase(minKey);
     heap.availableKey.push(minKey);
 }
 
-void addNode(NodeHeap heap, Node& newNode)
+void addNode(NodeHeap& heap, Node& newNode)
 {
     int newKey;
     if(heap.availableKey.size() == 0){newKey = heap.map.size();}
     else{newKey = heap.availableKey.top();heap.availableKey.pop();}
+    std::cout << "Adding: " << newKey << newNode.character << std::endl;
     heap.map.insert({newKey, newNode});
+}
+
+//helper print functions
+void ppNodeHeap(NodeHeap h)
+{
+    std::cout << h.map.size() << std::endl;
+    int len = h.map.size();
+    for(int i = 0; i< len-1; i++)
+    {
+        Node x;
+        x.character = '^';
+        x.frequency = -10;
+        popMin(h, x);
+        std::cout << x.character << " , " << x.frequency << " , " << x.isLeaf << '\n';
+    }
 }
 
 /**
@@ -76,21 +94,20 @@ int huffman_encode(const unsigned char *bufin,
         std::cout <<  n.isLeaf << ", " << n.character  << ", " << n.frequency << '\n';
     }
     NodeHeap nodeHeap;
-//    nodeHeap.availableKey = nodeMap.size();
     nodeHeap.map = nodeMap;
+    ppNodeHeap(nodeHeap);
     //3. pop the smallest two nodes;
-    for(int i = 0; i < nodeMap.size(); i++)
-    {
-        Node x;
-        popMin(nodeHeap, x);
-        std::cout << x.character << '\t' << x.frequency << ',' << x.isLeaf << '\n';
-    }
-    /*
     Node a, b;
     popMin(nodeHeap,a);
     popMin(nodeHeap,b);
-    std::cout << a.character;
-    */
+    Node c;
+    c.isLeaf = false;
+    c.character = '$';
+    c.frequency = a.frequency + b.frequency;
+    a.parent = &c;
+    b.parent = &c;
+    addNode(nodeHeap, c);
+    ppNodeHeap(nodeHeap);
 
     std::cout << "Finished Encoding Input File\n";
     return 1; 
