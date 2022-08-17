8c0c044b7dba47680f89efd7ebed57d4dd245721
Joey <joeypublicgit@gmail.com>
2022-08-17 05:09:52 +0000

    switch naive sobel to float math to remove integer math artifacts

--- assignments/3_assignment3/sobel/src/student_sobel.cpp
+++ assignments/3_assignment3/sobel/src/student_sobel.cpp
@@ -11,9 +11,9 @@ int sobel_kernel_x[KERNEL_SZ][KERNEL_SZ] = {
 	{ 1,  0, -1}};
 
 int sobel_kernel_y[KERNEL_SZ][KERNEL_SZ] = {
-	{ -1, -2, -1},
+	{ 1, 2, 1},
 	{ 0, 0, 0},
-	{1, 2, 1}};
+	{-1, -2, -1}};
 
 void sobel(const Mat& src, Mat& dst)
 { 
@@ -24,31 +24,49 @@ void sobel(const Mat& src, Mat& dst)
     //4. calculate the  
     const int rows = src.rows;
     const int cols = src.cols;
-    int result_x, result_y;
-    int ans[rows][cols];
+    float result_x, result_y;
+    float ans[rows][cols];
     for (int r = 1; r < rows-1; r++){
         for(int c = 1; c < cols-1; c++){
             result_x=0; result_y=0; 
             for(int i = 0; i < KERNEL_SZ; i++){
                 for(int j = 0; j < KERNEL_SZ; j++){
-                    result_x += sobel_kernel_x[i][j] * (int) src.at<unsigned char>(r-1+i,c-1+j);
-                    result_y += sobel_kernel_y[i][j] * (int) src.at<unsigned char>(r-1+i,c-1+j);
+                    result_x += (float) sobel_kernel_x[i][j] * (float) src.at<unsigned char>(r-1+i,c-1+j);
+                    result_y += (float) sobel_kernel_y[i][j] * (float) src.at<unsigned char>(r-1+i,c-1+j);
                 }
             }
         ans[r][c] = sqrt((result_x*result_x) + (result_y*result_y));
         }
     } 
-    dst = Mat(rows,cols,CV_32SC1,ans);
+    dst = Mat(rows,cols,CV_32FC1,ans);
     dst.convertTo(dst, CV_8U);
-    cout << dst << endl;
+//    cout << dst << endl;
     printf("\ndone!\n");
 }
 
 void sobel_unroll(const Mat& src, Mat& dst)
 {
-	// TODO
-    
     printf("Unrolled Sobel\n");
+    const int rows = src.rows;
+    const int cols = src.cols;
+    float result_x[4]={0};
+    float result_y[4]={0};
+    float ans[rows][cols];
+    for (int r = 1; r < rows-1; r++){
+        for(int c = 1; c < cols-1; c++){
+            result_x=0; result_y=0; 
+            for(int i = 0; i < KERNEL_SZ; i++){
+                for(int j = 0; j < KERNEL_SZ; j++){
+                    result_x += (float) sobel_kernel_x[i][j] * (float) src.at<unsigned char>(r-1+i,c-1+j);
+                    result_y += (float) sobel_kernel_y[i][j] * (float) src.at<unsigned char>(r-1+i,c-1+j);
+                }
+            }
+        ans[r][c] = sqrt((result_x*result_x) + (result_y*result_y));
+        }
+    } 
+    dst = Mat(rows,cols,CV_32FC1,ans);
+    dst.convertTo(dst, CV_8U);
+    printf("\ndone!\n");
 }
 
 void sobel_neon(const Mat& src, Mat& dst)
